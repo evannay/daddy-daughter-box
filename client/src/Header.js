df379a7fcd9cac1089ea4b2992704ffc563fe91e
Jeremy Bleak <Jeremybleak99@gmail.com>
2019-09-05 10:01:43 -0600

    header boi

---
+++ client/src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='hero-image'>
+            <h1 className='hero-title'>
+                Daddy Daughter Date Box!
+            </h1>
+        </div>
+    )
+}
+
+export default Header

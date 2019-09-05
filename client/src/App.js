5fd468a0d4ffff4d9e03bfd83efdecc9a6e323fa
Evan Nay <enay1248@gmail.com>
2019-09-05 10:28:20 -0600

    hotfixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Header from './Header'
+import About from '/About'
 import './App.css'
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
         <div className='app-container'>
             <Header />
             <Navbar />
+            <About />
         </div>
     )
 }

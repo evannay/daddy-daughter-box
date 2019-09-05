408cb16f45cb0af8c71e8448166b171f128fd515
Evan Nay <enay1248@gmail.com>
2019-09-05 10:51:09 -0600

    fixxxx

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,8 @@ import React from 'react'
 import Navbar from './Navbar'
 import Header from './Header'
 import About from './About'
-<<<<<<< HEAD
-=======
 import Cart from './Cart'
 import './App.css'
->>>>>>> cart
 
 const App = () => {
     return (

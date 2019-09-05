03e9f13eeb9dc74916ee6154ecccebf6fe136ea4
Evan Nay <enay1248@gmail.com>
2019-09-05 10:13:28 -0600

    about

---
+++ client/src/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className='about-container'>
+            This is our about component, WOO WOO!
+        </div>
+    )
+}
+
+export default About

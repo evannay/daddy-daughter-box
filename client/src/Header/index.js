f326245eac4571e8a93d80eff1024a24af7d2f49
Jeremy Bleak <Jeremybleak99@gmail.com>
2019-09-05 14:53:18 -0600

    headerStuffsss

--- client/src/Header/index.js
+++ client/src/Header/index.js
@@ -5,10 +5,11 @@ const Header = () => {
     return (
         <div className='hero-image'>
             <div className='hero-title'>
+                <img src="https://cdn.shopify.com/s/files/1/0018/1587/1546/products/custom-design-corrugated-mailer-boxes_1024x1024.png?v=1543205964" alt=""/>
             <h1>
-                Daddy Daughter Date Box
+                DADDY DAUGHTER DATE BOXES
             </h1>
-
+            <h3>Bond with you daughter you fool!</h3>
             </div>
         </div>
     )

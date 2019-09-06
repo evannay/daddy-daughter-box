50f8b49f3b503c2974487efa473083d11ba5cdeb
Jeremy Bleak <Jeremybleak99@gmail.com>
2019-09-06 15:03:17 -0600

    toggle

--- client/src/StoreFront/index.js
+++ client/src/StoreFront/index.js
@@ -10,23 +10,23 @@ const StoreFront = (props) => {
             <h1>Choose your time line</h1>
             <p>Your box will be selected from Theme choices</p>
             <div className = 'subscription-price-box'>
-                <div className='subsciption-box-option' onClick={() => { props.savedSubscription({ lenght: "1 month", price: 34.99 }) }}>
+                <div className={props.isSubscribed ? 'subsciption-box-option-selected' : "subsciption-box-option"} onClick={() => { props.savedSubscription({ lenght: "1 month", price: 34.99 }) }}>
                 <h3> 1 Month</h3>
                 <h1> 34.99</h1>
                 <p>a month</p>
             </div>
-                <div className='subsciption-box-option' onClick={() => { props.savedSubscription({ lenght: "3 month", price: 29.99 }) }}>
+                <div className={props.isSubscribed2 ? 'subsciption-box-option-selected' : "subsciption-box-option"} onClick={() => { props.savedSubscription2({ lenght: "3 month", price: 29.99 }) }}>
                 <h3> 3 Months</h3>
                 <h1>29.99</h1>
                 <p>a month</p>
             </div>
-                <div className='subsciption-box-option' onClick={() => { props.savedSubscription({ lenght: "6 month", price: 24.99 }) }}>
+                <div className={props.isSubscribed3 ? 'subsciption-box-option-selected' : "subsciption-box-option"} onClick={() => { props.savedSubscription3({ lenght: "6 month", price: 24.99 }) }}>
                 <h3> 6 Months</h3>
                 <h1>24.99</h1>
                 <p>a month</p>
 
             </div>
-                <div className='subsciption-box-option' onClick = {() => { props.savedSubscription({ lenght: "12 month", price: 19.99 }) }}>
+                <div className={props.isSubscribed4 ? 'subsciption-box-option-selected' : "subsciption-box-option"} onClick = {() => { props.savedSubscription4({ lenght: "12 month", price: 19.99 }) }}>
                 <h3> 12 Months</h3>
                 <h1>19.99</h1>
                 <p>a month</p>

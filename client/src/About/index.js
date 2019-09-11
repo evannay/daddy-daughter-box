92c99598770c0f859d14274eae2b62781d44854d
Jeremy Bleak <Jeremybleak99@gmail.com>
2019-09-11 14:05:31 -0600

    aboutagain

--- client/src/About/index.js
+++ client/src/About/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './about.css'
+import {Link} from 'react-router-dom'
 
 const About = () => {
     return (
@@ -8,10 +9,10 @@ const About = () => {
                 <div className='about-us-image'>
                 </div>
                 <div className='about-us-text'>
-                    <h1>Our Product</h1>
+                    <h1>About us and stuff</h1>
                     <p>some text to tell people about us Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui soluta voluptatem suscipit asperiores laborum alias. Nesciunt voluptatem est, quos repellendus maiores, tempore nulla asperiores nostrum accusantium cum odit voluptate dolores!
                     </p>
-                    <button>Get Started!</button>
+                    <Link to='/shop'><button className='get-started-button'>Get Started!</button></Link>
                 </div>
             </div>
             <div className='about-cover-title'>

b78259977bfa6f964c742819f71e072225d371a6
Evan Nay <enay1248@gmail.com>
2019-09-13 15:48:17 -0600

    deployable

--- server.js
+++ server.js
@@ -2,17 +2,24 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const PORT = 3334
+const path = require('path')
 
+app.use(express.static(path.join(__dirname, "client", "build")))
 app.use('/', express.json())
 app.use('/box', require('./routes/boxRoutes'))
 
-mongoose.connect('mongodb://localhost:27017/dddb-app', { useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/dddb-app', { useNewUrlParser: true })
     .then(() => {
         console.log('mongoose is connected')
     })
     .catch(err => {
         console.log(err)
     })
+
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(PORT, () => {
     console.log(`port is running on ${PORT}`)
